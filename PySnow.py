eef62a56536e1dd546aa0e4597bb041df99a2e82
Uriel-SG <65650311+Uriel-SG@users.noreply.github.com>
2022-09-30 21:05:24 -0300

    Add files via upload

--- PySnow.py
+++ PySnow.py
@@ -1,23 +1,44 @@
 import pyUnicodeSteganography as usteg
 import time
+import os
+import sys
+
 
 print("\n####### PySnow #######")
 time.sleep(0.5)
 
+absolutepath = os.path.abspath(__file__)
+
+fileDirectory = os.path.dirname(absolutepath)
+
+dirlist = os.listdir(fileDirectory)
+
+if "outputs" not in dirlist:
+	outputpath = fileDirectory + "/" + "outputs"
+	os.mkdir(outputpath)
+
 def snow():
+	global fileDirectory
 	print("\n1- Nascondi\n2- Estrai\n3- Esci\n")
 	scelta = str(input("--> "))
 	if scelta == "1":
 		try: 
-			text = input("Contenitore: ")
+			text = input("Percorso file Contenitore: ")
 			secret_msg = input("\nMessaggio segreto: ")
 			newfile = input("\nNuovo nome: ") + ".txt"
 			with open(text, "r") as dec:
 				contain = dec.read()
 			encoded = usteg.encode(contain, secret_msg)
-			with open(newfile, "w") as steg:
+			outputfolder = fileDirectory + "/outputs/"
+			outputpath = outputfolder + newfile
+			with open(outputpath, "w") as steg:
 				steg.write(encoded)
+			time.sleep(0.5)
 			print("\nFatto... Messaggio nascosto...")
+			time.sleep(0.5)
+			print(f"\nTroverai il nuovo file in {outputfolder}")
+			time.sleep(0.5)
+			print("\nDigita invio per tornare al menu iniziale")
 			input()
 			snow()
 		except ValueError:
@@ -30,7 +51,8 @@ def snow():
 			with open(file, "r") as steg:
 				message = steg.read()
 				decoded = usteg.decode(message)
-				print("Messaggio segreto:\n\n" + decoded)
+				time.sleep(0.5)
+				print("\nMessaggio segreto:\n\n" + decoded)
 				time.sleep(0.5)
 				input("\nDigita un tasto per continuare")
 				snow()
